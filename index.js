5b0fd8de2fd410fbbe430c4849cd09e530947cea
Iago-pixel <pristonvinicius@hotmail.com>
2023-05-12 18:34:09 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+let tasks = [
+  {
+    titulo: "Comprar comida para o gato",
+    tipo: "urgente",
+  },
+  {
+    titulo: "Limpar o quarto",
+    tipo: "urgente",
+  },
+  {
+    titulo: "Consertar Computador",
+    tipo: "prioritário",
+  },
+  {
+    titulo: "Guardar dinheiro do lanche",
+    tipo: "urgente",
+  },
+  {
+    titulo: "Beber água",
+    tipo: "prioritário",
+  },
+];
+
+const urgentes = tasks.filter((task) => task.tipo == "urgente");
+const prioritarios = tasks.filter((task) => task.tipo == "prioritário");
+const normais = tasks.filter((task) => task.tipo == "normal");
+
+const tasksOrganized = [...urgentes, ...prioritarios, ...normais];
+
+const tasksList = document.getElementById("tasks__list");
+
+for (let i = 0; i < tasksOrganized.length; i++) {
+  const li = document.createElement("li");
+
+  const div = document.createElement("div");
+  div.classList.add("task__infs");
+
+  const ball = document.createElement("div");
+  ball.classList.add("task__ball");
+
+  const span = document.createElement("span");
+  span.innerText = tasksOrganized[i].titulo;
+
+  div.appendChild(ball);
+  div.appendChild(span);
+
+  const button = document.createElement("button");
+  button.innerHTML = '<i class="fas fa-trash-alt"></i>';
+
+  li.appendChild(div);
+  li.appendChild(button);
+
+  tasksList.appendChild(li);
+
+  li.classList.add("task");
+
+  if (tasksOrganized[i].tipo == "normal") {
+    ball.classList.add("normal");
+  } else if (tasksOrganized[i].tipo == "prioritário") {
+    ball.classList.add("prioritario");
+  } else {
+    ball.classList.add("urgente");
+  }
+}
